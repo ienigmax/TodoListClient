cae79a5ed4dd653227298cde91d71e5c43b09744
iEnigmaX <alege92@outlook.com>
2018-09-30 18:49:44 +0300

    Add routes skeleton

--- app/router.js
+++ app/router.js
@@ -7,6 +7,12 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('tasks', function() {
+    this.route('add', {path: 'add'});
+    this.route('remove', {path: 'remove/:task_uuid'});
+    this.route('toggle', {path: ':task_uuid/:task_status'});
+  });
+  this.route('login');
 });
 
 export default Router;
