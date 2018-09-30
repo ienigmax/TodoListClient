64e5d144b7b40b1d999fca169e018c5ef85234fd
iEnigmaX <alege92@outlook.com>
2018-09-30 18:50:21 +0300

    Constants file

---
+++ app/constants.js
@@ -0,0 +1,9 @@
+export const API_CONSTS = {
+    API_HOST_ADDRESS: "http://todo-php.net/",
+    API_BASE_URI: "api/v1/tasks/",
+    TASK_GET_ALL: "get_all_tasks",
+    TASK_ADD_NEW: "add_new_task",
+    TASK_DELETE_OLD: "delete_task",
+    TASK_TOGGLE_STATUS: "change_status"
+
+};
