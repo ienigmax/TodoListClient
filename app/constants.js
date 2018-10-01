18926a184c6690e4f33d33c26daf29ef9f4c34c5
iEnigmaX <alege92@outlook.com>
2018-10-02 00:42:57 +0300

    change API host address

--- app/constants.js
+++ app/constants.js
@@ -1,5 +1,5 @@
 export const API_CONSTS = {
-    API_HOST_ADDRESS: "http://todo-php.net/",
+    API_HOST_ADDRESS: "http://localhost:8012/",
     API_BASE_URI: "api/v1/tasks/",
     TASK_GET_ALL: "get_all_tasks",
     TASK_ADD_NEW: "add_new_task",
