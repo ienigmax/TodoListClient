d873de68c5255c32e5701c9a2820effc0d1cc08b
iEnigmaX <alege92@outlook.com>
2018-10-02 19:58:19 +0300

    task remove functionality

---
+++ app/routes/actions/remove.js
@@ -0,0 +1,24 @@
+import Route from '@ember/routing/route';
+import fetch from "fetch";
+import {API_CONSTS} from "../../constants";
+
+export default Route.extend({
+  model() {
+    let { uuid } = this.paramsFor(this.routeName);
+    fetch(API_CONSTS.API_HOST_ADDRESS + API_CONSTS.API_BASE_URI + API_CONSTS.TASK_DELETE_OLD + '?uuid=' + uuid, {
+      method: 'GET',
+      mode: "cors",
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      credentials: "same-origin"
+    }).then((response) => {
+      console.log(response.json());
+      this.transitionTo('tasklist')
+    }).catch((error) => {
+      console.error(error.message);
+      this.transitionTo('tasklist')
+    });
+  }
+});
