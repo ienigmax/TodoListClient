d8ea7ba2c530bfeacc877358213ffda5cd5c0a9b
iEnigmaX <alege92@outlook.com>
2018-10-02 19:55:36 +0300

    remove old config code

--- config/environment.js
+++ config/environment.js
@@ -20,9 +20,6 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-      usingCors: false,
-      corsWithCreds: false,
-      apiURL: null
     }
   };
 
