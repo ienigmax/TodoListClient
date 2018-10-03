c0b6b94334ecaab79175a4ed99b8357ef8def4ef
iEnigmaX <alege92@outlook.com>
2018-10-03 21:32:36 +0300

    Add edit routes to the constants file

--- app/constants.js
+++ app/constants.js
@@ -4,6 +4,7 @@ export const API_CONSTS = {
     TASK_GET_ALL: "get_all_tasks",
     TASK_ADD_NEW: "add_new_task",
     TASK_DELETE_OLD: "delete_task",
-    TASK_TOGGLE_STATUS: "change_status"
-
+    TASK_TOGGLE_STATUS: "change_status",
+    TASK_EDIT_TITLE: "update_title",
+    TASK_EDIT_CONTENT: "update_content"
 };
