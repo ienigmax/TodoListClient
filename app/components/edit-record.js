29a6d92c1cfaf57333c9b4377ab89144f40b258f
iEnigmaX <alege92@outlook.com>
2018-10-03 21:40:56 +0300

    added functionality to encode the title and the content to base 64, as unicode or URI special characters may be inside the messages and ruin the application, added support to clear the input fields

--- app/components/edit-record.js
+++ app/components/edit-record.js
@@ -4,19 +4,24 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   router: service(),
   actions: {
-    addNewTask(){
+    resetFields(){
+      $('#title-inp-edit').val('');
+      $('#content-inp-edit').val('');
+      $('#tsk-uuid-hidden').val('');
+      $('#err-label-edit').text('');
+    },
+    editTask(){
 
-      let title = $('#title-inp').val(), content = $('#content-inp').val();
-      let escapeSpecialChars = (str) => {
-        return str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '%[]%')
-      }
+      let title = $('#title-inp-edit').val(), content = $('#content-inp-edit').val(), uuid = $('#tsk-uuid-hidden').val();
+
+      let Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 
       if((typeof title === 'undefined')||(title === '')){
-        $('#err-label').text('title missing');
+        $('#err-label-edit').text('title missing');
       } else if((typeof content === 'undefined')||(content === '')){
-        $('#err-label').text('content missing');
+        $('#err-label-edit').text('content missing');
       } else {
-        this.get('router').transitionTo('/actions/add/' + escapeSpecialChars(title) + '/' + escapeSpecialChars(content));
+        this.get('router').transitionTo('/actions/edit/' + uuid + '/' + Base64.encode(title) + '/' + Base64.encode(content));
       }
     },
   }
